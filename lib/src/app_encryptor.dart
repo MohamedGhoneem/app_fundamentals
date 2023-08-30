47e2d00ac579e60f6db609aa313ac4935dd31413
Mohamed Ghoneim <maghoneim@2p.com.sa>
2023-08-30 08:14:02 +0300

    encryptor

--- lib/src/app_encryptor.dart
+++ lib/src/app_encryptor.dart
@@ -3,36 +3,47 @@ import 'dart:developer';
 import 'dart:io';
 import 'package:encrypt/encrypt.dart';
 
-void main() {
-  const plainText = 'Sensitive data to be encrypted';
-  final key = Key.fromUtf8('Your 256-bit AES encryption key');
-  final iv = IV.fromLength(16); // Initialization vector (IV) for AES
+class AppEncryptor {
+  static const plainText = 'Sensitive data to be encrypted';
+  final key = Key.fromUtf8('key');
+  final iv = IV.fromLength(16);
+  late final encrypter; // Initialization vector (IV) for AES
+  init() {
+    encrypter = Encrypter(AES(key));
+  }
 
-  final encrypter = Encrypter(AES(key));
+  encryptData(data) {
+    // Encrypt data
+    final encryptedText = encrypter.encrypt(data, iv: iv);
+    log('Encrypted text: ${encryptedText.base64}');
+  }
 
-  // Encrypt data
-  final encryptedText = encrypter.encrypt(plainText, iv: iv);
-  log('Encrypted text: ${encryptedText.base64}');
+  decryptData(encryptedText) {
+    // Decrypt data
+    final decryptedText = encrypter.decrypt(encryptedText, iv: iv);
+    log('Decrypted text: $decryptedText');
+  }
 
-  // Decrypt data
-  final decryptedText = encrypter.decrypt(encryptedText, iv: iv);
-  log('Decrypted text: $decryptedText');
+  encryptFile() {
+    // Encrypt file
+    final inputFile = File('path/to/input/file.txt');
+    final outputFile = File('path/to/output/encrypted_file.txt');
 
-  // Encrypt file
-  final inputFile = File('path/to/input/file.txt');
-  final outputFile = File('path/to/output/encrypted_file.txt');
+    final inputBytes = inputFile.readAsBytesSync();
+    final encryptedBytes = encrypter.encryptBytes(inputBytes, iv: iv).bytes;
 
-  final inputBytes = inputFile.readAsBytesSync();
-  final encryptedBytes = encrypter.encryptBytes(inputBytes, iv: iv).bytes;
+    outputFile.writeAsBytesSync(encryptedBytes);
+    log('File encrypted and saved.');
+  }
 
-  outputFile.writeAsBytesSync(encryptedBytes);
-  log('File encrypted and saved.');
+  decryptFile(encryptedBytes) {
+    // Decrypt file
+    final decryptedBytes =
+        encrypter.decryptBytes(Encrypted(encryptedBytes), iv: iv);
+    const decryptedFilePath = 'path/to/output/decrypted_file.txt';
 
-  // Decrypt file
-  final decryptedBytes = encrypter.decryptBytes(Encrypted(encryptedBytes), iv: iv);
-  const decryptedFilePath = 'path/to/output/decrypted_file.txt';
-
-  final decryptedFile = File(decryptedFilePath);
-  decryptedFile.writeAsBytesSync(decryptedBytes);
-  log('File decrypted and saved at: $decryptedFilePath');
+    final decryptedFile = File(decryptedFilePath);
+    decryptedFile.writeAsBytesSync(decryptedBytes);
+    log('File decrypted and saved at: $decryptedFilePath');
+  }
 }
