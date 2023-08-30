23356c650beed4c64759f2780d36573395f84688
Mohamed Ghoneim <maghoneim@2p.com.sa>
2023-08-30 10:01:57 +0300

    encrypt

--- lib/src/app_encryptor.dart
+++ lib/src/app_encryptor.dart
@@ -1,51 +1,62 @@
-import 'dart:convert';
 import 'dart:developer';
-import 'dart:io';
 import 'package:encrypt/encrypt.dart';
 
-class AppEncryptor {
-  static const plainText = 'Sensitive data to be encrypted';
-  final key = Key.fromUtf8('key');
-  final iv = IV.fromLength(16);
-  late final encrypter; // Initialization vector (IV) for AES
+class Encryptor {
+  // static const plainText = 'Sensitive data to be encrypted';
+  final _key = Key.fromUtf8('TWOBEGHONEIMENCY');
+  final _iv = IV.fromLength(16);
+  late final _encrypter; // Initialization vector (IV) for AES
   init() {
-    encrypter = Encrypter(AES(key));
+    _encrypter = Encrypter(AES(_key));
   }
 
-  String? encryptData(data) {
+  encryptData(data) {
     // Encrypt data
-    final encryptedText = encrypter.encrypt(data, iv: iv);
+    final encryptedText = _encrypter.encrypt(data, iv: _iv);
     log('Encrypted text: ${encryptedText.base64}');
     return encryptedText;
   }
 
-  String? decryptData(encryptedText) {
+  decryptData(encryptedText) {
     // Decrypt data
-    final decryptedText = encrypter.decrypt(encryptedText, iv: iv);
+    final decryptedText = _encrypter.decrypt(encryptedText, iv: _iv);
     log('Decrypted text: $decryptedText');
     return decryptedText;
   }
 
-  encryptFile() {
-    // Encrypt file
-    final inputFile = File('path/to/input/file.txt');
-    final outputFile = File('path/to/output/encrypted_file.txt');
-
-    final inputBytes = inputFile.readAsBytesSync();
-    final encryptedBytes = encrypter.encryptBytes(inputBytes, iv: iv).bytes;
-
-    outputFile.writeAsBytesSync(encryptedBytes);
-    log('File encrypted and saved.');
-  }
-
-  decryptFile(encryptedBytes) {
-    // Decrypt file
-    final decryptedBytes =
-        encrypter.decryptBytes(Encrypted(encryptedBytes), iv: iv);
-    const decryptedFilePath = 'path/to/output/decrypted_file.txt';
-
-    final decryptedFile = File(decryptedFilePath);
-    decryptedFile.writeAsBytesSync(decryptedBytes);
-    log('File decrypted and saved at: $decryptedFilePath');
-  }
+// encryptFile(File file) {
+//   // Encrypt file
+//   // final inputFile = File('path/to/input/file.txt');
+//   String outPutFileName = file.path.split('/').last;
+//   print('outPutFileName : $outPutFileName');
+//   String outPutFilePath = file.path.replaceAll(outPutFileName, '');
+//   print('outPutFilePath : $outPutFilePath');
+//
+//   String extension = outPutFileName.split('.').last;
+//   print('outPutFileExtension : $extension');
+//
+//   outPutFileName = outPutFileName.replaceAll('.$extension', '');
+//   outPutFileName = outPutFileName.replaceAll(
+//       outPutFileName, '${outPutFileName}Encrypted.$extension');
+//   print('new outPutFileName : $outPutFileName');
+//
+//   final outputFile = File('$outPutFilePath$outPutFileName');
+//
+//   final inputBytes = file.readAsBytesSync();
+//   final encryptedBytes = _encrypter.encryptBytes(inputBytes, iv: _iv).bytes;
+//
+//   outputFile.writeAsBytesSync(encryptedBytes);
+//   log('File encrypted and saved.');
+// }
+//
+// decryptFile(encryptedBytes) {
+//   // Decrypt file
+//   final decryptedBytes =
+//       _encrypter.decryptBytes(Encrypted(encryptedBytes), iv: _iv);
+//   const decryptedFilePath = 'path/to/output/decrypted_file.txt';
+//
+//   final decryptedFile = File(decryptedFilePath);
+//   decryptedFile.writeAsBytesSync(decryptedBytes);
+//   log('File decrypted and saved at: $decryptedFilePath');
+// }
 }
