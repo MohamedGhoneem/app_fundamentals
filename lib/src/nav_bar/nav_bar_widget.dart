bbff68161f883efd43d866e4a1d764e6f193211b
Mohamed Ghoneim <maghoneim@2p.com.sa>
2023-09-17 15:54:57 +0300

    navbar

--- lib/src/nav_bar/nav_bar_widget.dart
+++ lib/src/nav_bar/nav_bar_widget.dart
@@ -13,23 +13,37 @@ class NavBarWidget extends StatelessWidget {
   final Color borderColor;
   final Color selectedColor;
   final Color unSelectedColor;
+  final FontWeight? selectedFontWeight;
+  final FontWeight? unSelectedFontWeight;
+  final BoxDecoration? selectedDecoration;
+  final BoxDecoration? unSelectedDecoration;
 
-  const NavBarWidget(
-      {Key? key,
-      required this.height,
-      required this.textFontSize,
-      required this.background,
-      required this.borderColor,
-      required this.selectedColor,
-      required this.unSelectedColor})
-      : super(key: key);
+  const NavBarWidget({
+    Key? key,
+    required this.height,
+    required this.textFontSize,
+    required this.background,
+    required this.borderColor,
+    required this.selectedColor,
+    required this.unSelectedColor,
+    this.selectedFontWeight,
+    this.unSelectedFontWeight,
+    this.selectedDecoration,
+    this.unSelectedDecoration,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: height + MediaQuery.of(context).viewPadding.bottom,
+      height: height + MediaQuery
+          .of(context)
+          .viewPadding
+          .bottom,
       padding:
-          EdgeInsets.only(bottom: MediaQuery.of(context).viewPadding.bottom),
+      EdgeInsets.only(bottom: MediaQuery
+          .of(context)
+          .viewPadding
+          .bottom),
       decoration: BoxDecoration(
           color: background,
           border: Border(top: BorderSide(color: borderColor, width: .7))),
@@ -41,15 +55,24 @@ class NavBarWidget extends StatelessWidget {
               children: <Widget>[
                 for (int i = 0; i < navBarBloc.naveBarItemList.length; i++)
                   Expanded(
-                      child: navBarItem(
-                          index: i,
-                          icon: snapshot.data == i
-                              ? navBarBloc.naveBarItemList[i].selectedIcon
-                              : navBarBloc.naveBarItemList[i].unSelectedIcon,
-                          title: navBarBloc.naveBarItemList[i].title,
-                          color: snapshot.data == i
-                              ? selectedColor
-                              : unSelectedColor)),
+                    child: navBarItem(
+                        index: i,
+                        icon: snapshot.data == i
+                            ? navBarBloc.naveBarItemList[i].selectedIcon
+                            : navBarBloc.naveBarItemList[i].unSelectedIcon,
+                        title: navBarBloc.naveBarItemList[i].title,
+                        color: snapshot.data == i
+                            ? selectedColor
+                            : unSelectedColor,
+                        fontWeight: snapshot.data == i
+                            ? selectedFontWeight ?? FontWeight.bold
+                            : unSelectedFontWeight ?? FontWeight.normal,
+                        decoration: snapshot.data == i
+                            ? selectedDecoration ?? const BoxDecoration()
+                            : unSelectedDecoration ?? const BoxDecoration(),
+                        topColor: snapshot.data == i
+                            ? selectedColor : Colors.transparent),
+                  ),
               ],
             );
           }),
@@ -61,24 +84,40 @@ class NavBarWidget extends StatelessWidget {
     required Widget? icon,
     required String? title,
     required Color color,
+    required FontWeight fontWeight,
+    required BoxDecoration decoration,
+    required Color topColor,
   }) {
     return InkWell(
-      onTap: () {
-        navBarBloc.pickItem(index);
-      },
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          if (icon != null) Flexible(child: icon),
-          if (icon != null) const SizedBox(height: 4),
-          AppText(
-            label: title ?? '',
-            style: TextStyle(fontSize: textFontSize, color: color),
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-          )
-        ],
-      ),
-    );
+        onTap: () {
+          navBarBloc.pickItem(index);
+        },
+        child: Container(
+          decoration: decoration,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                height: 2,
+                width: 100,
+                margin: const EdgeInsets.symmetric(horizontal: 8),
+                color: topColor,
+              ),
+             const SizedBox(height: 4),
+              if (icon != null) Flexible(child: icon),
+              if (icon != null) const SizedBox(height: 4),
+              AppText(
+                label: title ?? '',
+                style: TextStyle(
+                    fontSize: textFontSize,
+                    color: color,
+                    fontWeight: fontWeight),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              // Spacer()
+            ],
+          ),
+        ));
   }
 }
